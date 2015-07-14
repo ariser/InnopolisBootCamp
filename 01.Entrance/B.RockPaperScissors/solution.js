4f2d49761ab0c710cf5e9af62118fea34edd483c
ariser <ariser@ariser.ru>
2015-07-15 03:10:16 +0800

    redo using WebWorkers an console.log instead of DOM mutating (too expensive and slow)

--- 01.Entrance/B.RockPaperScissors/solution.js
+++ 01.Entrance/B.RockPaperScissors/solution.js
@@ -1,224 +1,28 @@
 (function () {
   'use strict';
 
-  // region constants
-  var ROCK     = 0,
-      PAPER    = 1,
-      SCISSORS = 2;
-
-  var STRATEGIES = [
-    [ROCK, ROCK, PAPER, SCISSORS],
-    [ROCK, PAPER, PAPER, SCISSORS],
-    [ROCK, PAPER, SCISSORS, SCISSORS]
-  ];
-
-  var STRATEGIES_NAMES = ['Rock', 'Paper', 'Scissors'];
-
-  var winnerMap = {};
-  winnerMap[ROCK] = {};
-  winnerMap[ROCK][ROCK] = 0;
-  winnerMap[ROCK][PAPER] = -1;
-  winnerMap[ROCK][SCISSORS] = 1;
-
-  winnerMap[PAPER] = {};
-  winnerMap[PAPER][ROCK] = 1;
-  winnerMap[PAPER][PAPER] = 0;
-  winnerMap[PAPER][SCISSORS] = -1;
-
-  winnerMap[SCISSORS] = {};
-  winnerMap[SCISSORS][ROCK] = -1;
-  winnerMap[SCISSORS][PAPER] = 1;
-  winnerMap[SCISSORS][SCISSORS] = 0;
-  // endregion
-
-  // region Player
-  function Player(name) {
-    if (!(this instanceof Player)) {
-      throw new Error('Missing "new" keyword');
-    }
-
-    if (arguments.length < 1) {
-      throw new Error('Not enough arguments to Player');
-    }
-
-    var strategyIndex = Math.floor(Math.random() * STRATEGIES.length),
-        strategy      = STRATEGIES[strategyIndex],
-        strategyName  = STRATEGIES_NAMES[strategyIndex];
-
-    this.strategy = strategy;
-    this.strategyName = strategyName;
-    this.name = name;
-  }
-
-  Player.prototype.getFigure = function () {
-    return this.strategy[Math.floor(Math.random() * this.strategy.length)];
-  };
-  // endregion
-
-  // region Game
-  function Game(playerOne, playerTwo) {
-    if (!(this instanceof Game)) {
-      throw new Error('Missing "new" keyword');
-    }
-
-    if (arguments.length < 2) {
-      throw new TypeError('Not enough arguments to Game');
-    }
-
-    this.playerOne = playerOne;
-    this.playerTwo = playerTwo;
-  }
-
-  Game.prototype.getWinner = function () {
-    var playerOneFigure = this.playerOne.getFigure(),
-        playerTwoFigure = this.playerTwo.getFigure();
-
-    var playerOneWins = winnerMap[playerOneFigure][playerTwoFigure];
-
-    if (playerOneWins > 0) {
-      return this.playerOne;
-    } else if (playerOneWins < 0) {
-      return this.playerTwo;
-    } else {
-      return undefined; // just for clarity
-    }
-  };
-  // endregion
-
-  // region Contest
-  function Contest(roundsNumber) {
-    if (!(this instanceof Contest)) {
-      throw new Error('Missing "new" keyword.');
-    }
-
-    if (arguments.length < 1) {
-      throw new TypeError('Not enough arguments to Contest');
-    }
-
-    if (isNaN(roundsNumber) || roundsNumber === '') {
-      throw new TypeError('Rounds number must be... a number');
-    }
-
-    this.roundsNumber = parseInt(roundsNumber);
-    this.players = [];
-
-    this.observers = {};
-    this.roundsPlayed = 0;
-  }
-
-  // region observable contest
-  Contest.prototype.notify = function (event/*[, params...]*/) {
-    var eventObservers, index, length, params;
-
-    eventObservers = this.observers[event];
-    index = 0;
-    length = eventObservers.length;
-
-    params = Array.prototype.slice.call(arguments, 1);
-
-    for (; index < length; index += 1) {
-      eventObservers[index].apply(null, params);
-    }
-  };
-  Contest.prototype.subscribe = function (event, observer) {
-    if (typeof this.observers[event] === 'undefined') {
-      this.observers[event] = [];
-    }
-    this.observers[event].push(observer);
-  };
-  // endregion
-
-  Contest.prototype.play = function () {
-    this._initPlayers();
-
-    while (this.players.length > 1) {
-      this._playRound();
-    }
-  };
-
-  Contest.prototype._initPlayers = function () {
-    var playersNumber = Math.pow(2, this.roundsNumber);
-    for (var i = 0; i < playersNumber; i += 1) {
-      this.players.push(new Player(i + 1));
-    }
-
-    this.notify('playersInited', this.players);
-  };
-
-  Contest.prototype._playRound = function () {
-    this.roundsPlayed += 1;
-
-    this.notify('roundStart', this.roundsPlayed, this.players.length === 2);
-
-    var playersNumber = this.players.length,
-        roundWinners  = [];
-
-    for (var i = 0; i < playersNumber; i += 2) {
-      var playerOne = this.players[i],
-          playerTwo = this.players[i + 1];
-
-      var winner = this._getRoundWinner(playerOne, playerTwo);
-      roundWinners.push(winner);
-
-      this.notify('roundPlayed', playerOne, playerTwo, winner);
-    }
-
-    this.players = this.players.filter(function (player) {
-      return roundWinners.indexOf(player) >= 0;
-    });
-  };
-
-  Contest.prototype._getRoundWinner = function (playerOne, playerTwo) {
-    var playerOneVictories = 0,
-        playerTwoVictories = 0,
-
-        game               = new Game(playerOne, playerTwo);
-
-    while (Math.abs(playerOneVictories - playerTwoVictories) < 2) {
-      if (game.getWinner() === playerOne) {
-        playerOneVictories += 1;
-      } else if (game.getWinner() === playerTwo) {
-        playerTwoVictories += 1;
-      }
-    }
-
-    return playerOneVictories > playerTwoVictories ? playerOne : playerTwo;
-  };
-  // endregion
-
   window.onload = function () {
-    var form   = document.forms.contestForm,
-        input  = form.K,
-        output = form.out;
+    var form            = document.forms.contestForm,
+        input           = form.K,
+        output          = form.out,
+
+        worker;
+
+    worker = new Worker('contestWebWorker.js?' + Math.random());
+
+    worker.onmessage = function (e) {
+      if (e.data === 'done') {
+        output.innerHTML = 'Done. Please see the result in the browser console (Ctrl+Shift+I).';
+      } else {
+        output.innerHTML = e.data + '<br>';
+      }
+    };
 
     form.addEventListener('submit', function (e) {
       e.preventDefault();
 
-      output.innerHTML = '';
-
-      try {
-        var contest = new Contest(input.value);
-
-        contest.subscribe('playersInited', function (players) {
-          output.innerHTML += '<p>' +
-          players.map(function (player) {
-            return 'Player ' + player.name + ': ' + player.strategyName;
-          }).join('<br>') +
-          '</p>';
-        });
-
-        contest.subscribe('roundStart', function (roundIndex, isLast) {
-          output.innerHTML += isLast ? '<h4>Final</h4>' : '<h4>Round ' + roundIndex + '</h4>';
-        });
-
-        contest.subscribe('roundPlayed', function (playerOne, playerTwo, winner) {
-          output.innerHTML += '<p>Player ' + playerOne.name + ' vs Player ' + playerTwo.name + '. Player ' + winner.name + ' wins!';
-        });
-
-        contest.play();
-      } catch (e) {
-        output.innerHTML = e.message;
-      }
+      output.innerHTML = 'Playing...';
+      worker.postMessage(input.value);
     });
   }
 })();
